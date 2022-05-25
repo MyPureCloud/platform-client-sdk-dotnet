eb7785f65bc4c78fd08dd5df944950f70b084697
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2022-05-25 15:12:39 +0000

    145.0.2

--- build/src/PureCloudPlatform.Client.V2/Client/ApiClient.cs
+++ build/src/PureCloudPlatform.Client.V2/Client/ApiClient.cs
@@ -221,7 +221,7 @@ namespace PureCloudPlatform.Client.V2.Client
             RestClient.UserAgent = Configuration.UserAgent;
 
             // Set SDK version
-            request.AddHeader("purecloud-sdk", "145.0.1");
+            request.AddHeader("purecloud-sdk", "145.0.2");
 
             Retry retry = new Retry(this.RetryConfig);
             IRestResponse response;
