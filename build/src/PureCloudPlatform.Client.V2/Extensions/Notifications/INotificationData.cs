cddac6410a42be829804e2510010fea3a4c7dbdd
Tim Smith <tim.j.smith@genesys.com>
2024-09-05 16:37:39 -0600

    pages setup

--- build/src/PureCloudPlatform.Client.V2/Extensions/Notifications/INotificationData.cs
+++
@@ -1,9 +0,0 @@
-namespace PureCloudPlatform.Client.V2.Extensions.Notifications
-{
-    public interface INotificationData
-    {
-        string TopicName { get; set; }
-        string Version { get; set; }
-        Metadata Metadata { get; set; }
-    }
-}
